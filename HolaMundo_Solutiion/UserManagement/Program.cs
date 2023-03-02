79d0b3f97ec98232eb7386bff3ec0b905ad6e1d1
Abraham Chavez Castillo <abraham_cha88@hotmail.com>
2023-03-02 00:02:22 -0600

    Ajustes en código

--- HolaMundo_Solutiion/UserManagement/Program.cs
+++ HolaMundo_Solutiion/UserManagement/Program.cs
@@ -1,13 +1,10 @@
-﻿using System.Numerics;
-using Finanzas.CursoVisualStudio.Shared.DTOs;
-
-namespace UserManagement
+﻿namespace UserManagement
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            /*bool finishExecution = false;
+            bool finishExecution = false;
             UserRepo uRepo = new UserRepo();
             do
             {
@@ -29,9 +26,9 @@ namespace UserManagement
                         Console.WriteLine("Por favor ingrese el apodo o correo electrónico del usuario a buscar");
                         String searchValue = Console.ReadLine();
 
-                        var matches = uRepo.SearchUser(searchValue);
+                        var result = uRepo.SearchUser(searchValue);
                         Console.WriteLine("Los usuarios que empatan con el criterio de búsqueda son:");
-                        foreach (var item in matches)
+                        foreach (var item in result)
                         {
                             Console.WriteLine(item.ToString());
                         }
@@ -111,72 +108,72 @@ namespace UserManagement
                 }
 
             } while (!finishExecution);
-            //while (finishExecution == false);*/
+            //while (finishExecution == false);
 
-            //GenericRepo<User> uRepo =
-            //    new GenericRepo<User>();
+            GenericRepo<User> uRepoG =
+                new GenericRepo<User>();
 
-            //uRepo.Add(new User()
-            //{
-            //    ID = 3,
-            //    Email = "c@h.com",
-            //    NickName = "c",
-            //    Password = "ldfjij"
-            //});
+            uRepoG.Add(new User()
+            {
+                ID = 3,
+                Email = "c@h.com",
+                NickName = "c",
+                Password = "ldfjij"
+            });
 
-            //uRepo.Add(new User()
-            //{
-            //    ID = 1,
-            //    Email = "a@h.com",
-            //    NickName = "a",
-            //    Password = "a"
-            //});
-            //uRepo.Add(new User()
-            //{
-            //    ID = 2,
-            //    Email = "b@h.com",
-            //    NickName = "b",
-            //    Password = "poiut"
-            //});
+            uRepoG.Add(new User()
+            {
+                ID = 1,
+                Email = "a@h.com",
+                NickName = "a",
+                Password = "a"
+            });
+            uRepoG.Add(new User()
+            {
+                ID = 2,
+                Email = "b@h.com",
+                NickName = "b",
+                Password = "poiut"
+            });
 
-            //uRepo.Modify(new User()
-            //{
-            //    ID = 1,
-            //    Email = "a@hotmail.com",
-            //    NickName = "Abraham",
-            //    Password = "Admin123"
-            //},
-            //u => u.ID == 1,
-            //(newU, current) =>
-            //{
-            //    current.Email = newU.Email;
-            //    current.NickName = newU.NickName;
-            //    current.Password = newU.Password;
-            //});
+            uRepoG.Modify(new User()
+            {
+                ID = 1,
+                Email = "a@hotmail.com",
+                NickName = "Abraham",
+                Password = "Admin123"
+            },
+            u => u.ID == 1,
+            (newU, current) =>
+            {
+                current.Email = newU.Email;
+                current.NickName = newU.NickName;
+                current.Password = newU.Password;
+            });
 
-            //var matches = uRepo.Search(u => u.NickName.ToLower().Contains("h") && u.Email.ToLower().Contains("h"));
-            //uRepo.Search(SearchUser);
+            var matches = uRepoG.Search(u => u.NickName.ToLower().Contains("h") && u.Email.ToLower().Contains("h"));
+            uRepoG.Search(SearchUser);
 
-            //matches = uRepo.Search(u =>
-            //u.NickName.ToLower().Contains("h"));
-            //matches = uRepo.Search(u =>
-            //u.Email.ToLower().Contains("h"));
-            //matches = uRepo.Search(u =>
-            //u.Password.ToLower().Contains("h"));
+            matches = uRepoG.Search(u =>
+            u.NickName.ToLower().Contains("h"));
+            matches = uRepoG.Search(u =>
+            u.Email.ToLower().Contains("h"));
+            matches = uRepoG.Search(u =>
+            u.Password.ToLower().Contains("h"));
 
-            ////uRepo.Delete(uRepo.Search(u => u.ID == 1).First());
+            //uRepo.Delete(uRepoG.Search(u => u.ID == 1).First());
 
-            //uRepo.Sort((u1, u2) =>
-            //{
-            //    return u1.Password.CompareTo(u2.Password);
-            //});
+            uRepoG.Sort((u1, u2) =>
+            {
+                return u1.Password.CompareTo(u2.Password);
+            });
 
-            //uRepo.Sort(y: 45, predicate: (u1, u2) =>
-            //{
-            //    return u1.Email.CompareTo(u2.Email);
-            //}, isAsc: false, x: 10, w: "Adios");
+            uRepoG.Sort(y: 45, predicate: (u1, u2) =>
+            {
+                return u1.Email.CompareTo(u2.Email);
+            }, isAsc: false, x: 10, w: "Adios");
 
-            //GenericRepo<Module> mRepo = new GenericRepo<Module>();
+            GenericRepo<Module> mRepo = new GenericRepo<Module>();
         }
 
         public static bool SearchUser(User u)
@@ -199,6 +196,296 @@ namespace UserManagement
             }
 
             return tmp;
+        }
+    }
+    public class UserRepo
+    {
+        private List<User> users;
+
+        public UserRepo()
+        {
+            this.users = new List<User>();
+        }
+
+        public User AddUser(User user)
+        {
+            try
+            {
+                this.users.Add(user);
+
+                return user;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public List<User> SearchUser(String searchValue)
+        {
+            try
+            {
+                if (String.IsNullOrEmpty(searchValue) == true)
+                {
+                    return this.users;
+                }
+
+                searchValue = searchValue.ToLower();
+
+                return this.users.Where(
+                u => u.NickName.ToLower().Contains(searchValue) ||
+                u.Email.ToLower().Contains(searchValue)).ToList();
+
+                //return (from u in this.users
+                //        where u.NickName.ToLower().Contains(searchValue) ||
+                //        u.Email.ToLower().Contains(searchValue)
+                //        select u).ToList();
+
+                //List<User> matches = new List<User>();
+                //foreach (User user in this.users)
+                //{
+                //    if (user.Email.ToLower().Contains(searchValue.ToLower()) == true || user.NickName.ToLower().Contains(searchValue.ToLower()) == true)
+                //    {
+                //        matches.Add(user);
+                //    }
+                //}
+
+                //return matches;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public User ModifyUser(User user)
+        {
+            try
+            {
+                var matches = this.SearchUser(user.Email);
+
+                if (matches.Any() == true)
+                {
+                    User tmp = matches.First();
+                    tmp.NickName = String.IsNullOrEmpty(user.NickName) == true ?
+                        tmp.NickName : user.NickName;
+                    tmp.Password = String.IsNullOrEmpty(user.Password) == true ? tmp.Password : user.Password;
+
+                    return matches.First();
+                }
+                else
+                {
+                    throw new Exception("No se encontró el usuario solicitado");
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public User DeleteUser(String email)
+        {
+            try
+            {
+                var matches = this.SearchUser(email);
+
+                if (matches.Any() == true)
+                {
+                    User tmp = matches.First();
+                    this.users.Remove(tmp);
+
+                    return tmp;
+                }
+                else
+                {
+                    throw new Exception("No hay elementos a eliminar");
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+
+    public class GenericRepo<T> : IGenericRepo<T>
+    {
+        private List<T> items;
+
+        public GenericRepo()
+        {
+            this.items = new List<T>();
+        }
+
+        public T Add(T item)
+        {
+            try
+            {
+                this.items.Add(item);
+
+                return item;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public T Delete(T entity)
+        {
+            try
+            {
+                this.items.Remove(entity);
+
+                return entity;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public T Modify(T item,
+            Func<T, bool> predicateSearch,
+            Action<T, T> predicateMod)
+        {
+            try
+            {
+                T current =
+                    this.Search(predicateSearch).First();
+                predicateMod(item, current);
+
+                return current;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public List<T> Search(Func<T, bool> predicate)
+        {
+            try
+            {
+                return this.items.Where(predicate).ToList();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public void Sort(Func<T, T, int> predicate,
+            bool isAsc = true,
+            int x = 0, float y = 9, String w = "Hola")
+        {
+            T pivot;
+
+            for (int a = 1; a < this.items.Count; a++)
+                for (int b = this.items.Count - 1; b >= a; b--)
+                {
+                    if (predicate(this.items[b - 1], this.items[b])
+                        == (isAsc == true ? 1 : -1))
+                    {
+                        pivot = this.items[b - 1];
+                        this.items[b - 1] = this.items[b];
+                        this.items[b] = pivot;
+                    }
+                }
+        }
+    }
+
+    public interface IGenericRepo<T>
+    {
+        public T Add(T item);
+
+        public List<T> Search(Func<T, bool> predicate);
+
+        public T Modify(T item,
+            Func<T, bool> predicateSearch,
+            Action<T, T> predicateMod);
+
+        public T Delete(T entity);
+
+        public void Sort(Func<T, T, int> predicate,
+            bool isAsc = true,
+            int x = 0, float y = 9, String w = "Hola");
+    }
+
+    /// <summary>
+    /// Clase que representa a  un Usuario
+    /// </summary>
+    public class User : IComparable<User>
+    {
+        #region Properties
+        /// <summary>
+        /// ID de tipo entero que debe ser único
+        /// </summary>
+        public int ID { get; set; }
+        public String Email { get; set; }
+        public String NickName { get; set; }
+        public String Password { get; set; }
+        #endregion
+
+        #region Methods
+        //CompareTo regresa 0 si ambos ojetos son iguales
+        //1 = si el objeto de la izquierda es mayor
+        //-1 = si el objeto de la izquierda es menor
+        public int CompareTo(User? other)
+        {
+            if (other == null)
+                return -1;
+
+            return this.ID.CompareTo(other.ID);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj != null && obj.GetType() == typeof(User))
+            {
+                User tmp = (User)obj;
+                return this.ID.Equals(tmp.ID);
+            }
+
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return $"Apodo: {this.NickName} - Correo electrónico: {this.Email}";
+        }
+        #endregion
+    }
+
+    public class Module : IComparable<Module>
+    {
+        public int ID { get; set; }
+        public String Name { get; set; }
+        public String Description { get; set; }
+
+        public int CompareTo(Module? other)
+        {
+            if (other == null)
+                return -1;
+
+            return this.ID.CompareTo(other.ID);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj != null && obj.GetType() == typeof(Module))
+            {
+                Module tmp = (Module)obj;
+                return this.ID.Equals(tmp.ID);
+            }
+
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {this.Description} - Nombre: {this.Name}";
         }
     }
 }
