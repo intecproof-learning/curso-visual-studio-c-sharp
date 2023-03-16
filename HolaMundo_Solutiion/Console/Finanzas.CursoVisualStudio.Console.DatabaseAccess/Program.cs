5eb2174692c9a0a879404a11d3a426bcd2c42061
Abraham Chavez Castillo <abraham_cha88@hotmail.com>
2023-03-16 15:25:10 -0600

    Ejercicio en clase

--- HolaMundo_Solutiion/Console/Finanzas.CursoVisualStudio.Console.DatabaseAccess/Program.cs
+++ HolaMundo_Solutiion/Console/Finanzas.CursoVisualStudio.Console.DatabaseAccess/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Finanzas.CursoVisualStudio.DataAccess.SQLDatabase.Models;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 
 namespace Finanzas.CursoVisualStudio.DatabaseAccess
@@ -7,8 +8,77 @@ namespace Finanzas.CursoVisualStudio.DatabaseAccess
     {
         static void Main(string[] args)
         {
-            QueryUsers();
-            QueryModules();
+            //QueryUsers();
+            //QueryModules();
+            QueryADO();
+        }
+
+        private static void QueryADO()
+        {
+            int idUser = 2;
+            string connS = "Server=localhost;Database=CursoVisualC#;User ID=sa;Password=Admin123;Trusted_Connection=False;Encrypt=False;MultipleActiveResultSets=true";
+            string queryS = $"SELECT * FROM [User]";
+            SqlConnection conn =
+                new SqlConnection(connS);
+            SqlCommand command = null;
+            SqlDataReader reader = null;
+            try
+            {
+                queryS = "SELECT * FROM [User]" +
+                    "WHERE id = @idUser";
+                command = new SqlCommand
+                    (queryS, conn);
+                command.Parameters
+                    .AddWithValue("@idUser", idUser);
+
+                conn.Open();
+                reader = command.ExecuteReader();
+                ///ExecuteReader sirve para leer el conjunto
+                ///de resultados que arroja la consulta
+                ///ExecuteNonQuery sirve para ejecutar
+                ///sentencias que no regresan un conjunto
+                ///de resultados, como insert, update
+                ///CREATE TABLE,etc
+                //command.ExecuteNonQuery();
+                while (reader.Read() == true)
+                {
+                    Console.WriteLine($"{reader[0]}-" +
+                    $"{reader[1]}-{reader["email"]}-" +
+                    $"{reader["password"]}");
+
+                    int idU = reader.GetSqlInt32(0).IsNull ? 0 :
+                        Convert.ToInt32(reader.GetSqlInt32(
+                            reader.GetOrdinal("id")).Value);
+                    Console.WriteLine($"El Id del usuario es:" +
+                        $"{idU}");
+                    string nickName = reader.GetSqlString(1).IsNull?
+                    String.Empty:Convert.ToString(reader
+                    .GetSqlString(1).Value);
+
+                    Console.WriteLine($"El NickName del usuario es:" +
+                        $"{nickName}");
+                }
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+            }
+            finally
+            {
+                if (command != null)
+                {
+                    command.Dispose();
+                }
+                if (reader != null)
+                {
+                    reader.Close();
+                    reader.DisposeAsync()
+                    .GetAwaiter()
+                    .GetResult();
+                }
+                conn.Close();
+                conn.Dispose();
+            }
         }
 
         private static void QueryUsers()
@@ -111,7 +181,7 @@ namespace Finanzas.CursoVisualStudio.DatabaseAccess
                     m = umrel.module,
                     u = u,
                     isActive = umrel.umr.IsActive
-                }).OrderByDescending(ob=>ob.m.Id);
+                }).OrderByDescending(ob => ob.m.Id);
 
                 foreach (var item in umRels)
                 {
