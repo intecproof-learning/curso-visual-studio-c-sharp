82691dab7e838fcf232042cc1f317df94e964fe4
Abraham Chavez Castillo <abraham_cha88@hotmail.com>
2023-04-03 17:51:05 -0600

    Ejercicio en clase

--- HolaMundo_Solutiion/WForms/Finanzas.CursoVisualStudio.Forms.Demo/Finanzas.cs
+++ HolaMundo_Solutiion/WForms/Finanzas.CursoVisualStudio.Forms.Demo/Finanzas.cs
@@ -1,32 +1,43 @@
 ﻿using Finanzas.CursoVisualStudio.Shared.Utilities;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace Finanzas.CursoVisualStudio.Forms.Demo
 {
     public partial class Finanzas : Form
     {
+        private DemoForm modules;
+        private DemoForm users;
+
         public Finanzas(String session)
         {
             Utilities.session = session;
             InitializeComponent();
+
+            this.modules = new DemoForm();
+            this.users = new DemoForm();
         }
 
         private void mnsUsersSubItem_Click(object sender, EventArgs e)
         {
+            if (this.users.IsDisposed == true)
+            {
+                this.users = new DemoForm();
+            }
 
+            this.users.MdiParent = this;
+            this.users.WindowState = FormWindowState.Maximized;
+            this.users.Show();
         }
 
         private void mnsModulesSubItem_Click(object sender, EventArgs e)
         {
+            if (this.modules.IsDisposed == true)
+            {
+                this.modules = new DemoForm();
+            }
 
+            this.modules.MdiParent = this;
+            this.modules.WindowState = FormWindowState.Maximized;
+            this.modules.Show();
         }
     }
 }
